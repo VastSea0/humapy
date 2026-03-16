fe38135c0dea02df3662f59119fdf78ea9fde535
VastSea0 <vastseaoffical0@outlook.com>
2026-03-16 04:11:38 +0300

    feat: Add interpreter module and integrate it into `main.rs` to execute parsed code.

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,36 @@ mod token;
 mod lexer;
 mod ast;
 mod parser;
+mod interpreter;
 
 use lexer::Lexer;
 use parser::Parser;
+use interpreter::Yorumlayici;
+use std::io::{self, Write};
 
 fn main() {
-    println!("Hüma Ayrıştırıcı (Parser) Testi");
-    println!("---------------------------");
-
+    println!("Hüma Programlama Dili");
+    println!("---------------------");
+    
     let kod = r#"
-        değişken x = 10;
-        eğer x > 5 {
-            yazdır("Büyük");
+        değişken isim = "Hüma";
+        değişken sürüm = 0.1;
+
+        yazdır("Hoş geldiniz! Bu projenin adı: " + isim);
+        yazdır("Mevcut sürüm: " + sürüm);
+        
+        değişken x = 15;
+        eğer x > 10 {
+            yazdır("x değeri 10'dan büyük.");
         } değilse {
-            yazdır("Küçük");
+            yazdır("x değeri 10'dan küçük veya eşit.");
         }
     "#;
 
     let tarayici = Lexer::new(kod);
     let mut ayristirici = Parser::new(tarayici);
-
     let program = ayristirici.parse_program();
 
-    for komut in program {
-        println!("{:#?}", komut);
-    }
+    let mut yorumlayici = Yorumlayici::new();
+    yorumlayici.yorumla(program);
 }
